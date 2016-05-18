eebd17dffec215366402bfd4fd816afb641f0b35
iris rotich <iriskocy@gmail.com>
2016-05-18 14:43:13 +0000

    Initial commit of the PHP tutorial.

---
+++ student.php
@@ -0,0 +1,43 @@
+<?php
+class Student{
+    public $Firstname;
+    public $Secondname;
+    private $Course;
+    public $Gender;
+    private $fullName;
+    private $genderName;
+    
+    private function setCourse()
+    {
+        return $this->Course = "Biology";
+    }
+    
+    public function getFullName()
+    {
+     return $this->fullName = $this->firstname." ".$this->Secondname;   
+    }
+    
+    public function getGenderName()
+    {
+        return $this->genderName = $this->Gender." ".$this->getFullName(); 
+    }
+    
+    
+    public function getCourse()
+    {
+        return $this->Course;
+    }
+
+    
+}
+
+$student = new Student();
+
+$student->firstname = "Bob";
+$student->Secondname = "Jones";
+$student->setCourse();
+$student->Gender = "Male";
+echo "\n";
+echo $student->getFullName();
+echo "\n";
+echo $student->getGenderName();
